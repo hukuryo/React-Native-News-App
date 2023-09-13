fe628639a004b697dc14960576368f571edb95eb
hukuryo <ryohei.1998.05.09@gmail.com>
2023-09-13 23:14:41 +0900

    ニュース記事のコンポーネントを作成

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import { ListItem } from './components/ListItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up to start working on your app!</Text>
+      <ListItem imageUrl={"https://picsum.photos/id/237/200/300"} 
+        title={"サンプルのテキスト1です。asssssaasssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"} 
+        author={"sample1"}
+      />
+      <ListItem imageUrl={"https://picsum.photos/seed/picsum/200/300"} 
+        title={"サンプルのテキスト2です。asssssaasssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"} 
+        author={"sample2"}
+      />
+      <ListItem imageUrl={"https://picsum.photos/200/300?grayscale"} 
+        title={"サンプルのテキスト3です。asssssaasssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"} 
+        author={"sample3"}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    flexDirection: "row",
+    backgroundColor: "white"
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: "space-between"
+  },
+  text: {
+    fontWeight: "bold"
+  },
+  subText: {
+    fontSize: 12
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,48 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+export const ListItem = (props) => {
+    const { imageUrl, title, author } = props
+    return (
+        <View style={styles.itemContainer}>
+            <View style={styles.leftContainer}>
+            <Image
+                style={{width: 100, height: 100}}
+                source={{uri: imageUrl}}
+            >
+            </Image>
+            </View>
+            <View style={styles.rightContainer}>
+            <Text style={styles.text} numberOfLines={3}>
+                {title}
+            </Text>
+            <Text style={styles.subText}>
+                {author}
+            </Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    itemContainer: {
+        height: 100,
+        width: "100%",
+        flexDirection: "row",
+        backgroundColor: "white",
+        marginBottom: 10
+    },
+    leftContainer: {
+        width: 100,
+    },
+    rightContainer: {
+        flex: 1,
+        padding: 10,
+        justifyContent: "space-between"
+    },
+    text: {
+        fontWeight: "bold"
+    },
+    subText: {
+        fontSize: 12
+    }
+});
