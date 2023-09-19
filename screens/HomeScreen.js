5e591290672bd2d27a046bf4a39dbaeb4bb98938
hukuryo <ryohei.1998.05.09@gmail.com>
2023-09-20 00:15:52 +0900

    ニュースの一覧を表示できるように修正

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,65 +1,65 @@
-import { FlatList, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
 import { ListItem } from '../components/ListItem';
-import { useEffect, useState } from 'react';
-import Constants from "expo-constants";
 import axios from 'axios';
+import Constants from "expo-constants";
 
-const URL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${Constants.extra.newsApiKey}`;
+const URL = `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${Constants.manifest2.extra.expoClient.extra.newsApiKey}`;
 
-export const HomeScreen = () => {
-const [ articles, setArticles ] = useState([]);
+const HomeScreen = () => {
+    const [articles, setArticles] = useState([]);
 
-const fetchArticles = async () => {
-    try{
-        const response = await axios.get(URL);
-        setArticles(response.data.articles);
-    }catch(error){
-        console.log(error);
+    const fetchArticles = async () => {
+        try {
+            const response = await axios.get(URL);
+            setArticles(response.data.articles);
+        } catch (error) {
+            console.log(error);
+        }
     }
-    setArticles(response);
-}
 
-useEffect(() => {
-    fetchArticles()
-}, [])
+    useEffect(() => {
+        fetchArticles();
+    }, [])
 
-return (
-    <SafeAreaView style={styles.container}>
-        <FlatList
-            data={articles}
-            renderItem={({item}) => (
-            <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author}/>
-            )}
-            keyExtractor={(item, index) => index.toString()}
-        >
-        </FlatList>
-    </SafeAreaView>
-);
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => (
+                    <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    backgroundColor: '#eee'
-},
-itemContainer: {
-    height: 100,
-    width: "100%",
-    flexDirection: "row",
-    backgroundColor: "white"
-},
-leftContainer: {
-    width: 100,
-},
-rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between"
-},
-text: {
-    fontWeight: "bold"
-},
-subText: {
-    fontSize: 12
-}
+    container: {
+        flex: 1,
+        backgroundColor: '#eee'
+    },
+    itemContainer: {
+        height: 100,
+        width: "100%",
+        flexDirection: "row",
+        backgroundColor: "white"
+    },
+    leftContainer: {
+        width: 100,
+    },
+    rightContainer: {
+        flex: 1,
+        padding: 10,
+        justifyContent: "space-between"
+    },
+    text: {
+        fontWeight: "bold"
+    },
+    subText: {
+        fontSize: 12
+    }
 });
+
+export default HomeScreen; // コンポーネントをデフォルトエクスポート
